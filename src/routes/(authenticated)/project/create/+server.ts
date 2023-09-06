3cdf47aa041ed6585a6591adc64897da5c3b8c15
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-09-05 20:51:00 -0400

    stff

--- src/routes/(authenticated)/project/create/+server.ts
+++ src/routes/(authenticated)/project/create/+server.ts
@@ -2,27 +2,26 @@
 import students from '@client/data/generated/students.json';
 import { user } from "@/client/stores/user";
 
-export const POST = {
+export const POST = 
   async ({request}) => {
     const formData = await request.formData();
-        const mentorNames = formData.get('mentor').split(" ")
-        const name = user.name.split(' ');
-        const student = students.find(s => s.firstName == name[0] && s.lastName == name[1] )
-        const projectObject = {
-            studentId: student.studentId,
-            subject: formData.get('subject'),
-            year: new Date().getFullYear(), 
-            tags: formData.getAll('selected'),
-            mentor: {
-              firstName: mentorNames[0],
-              lastName: mentorNames[1],
-              organization: formData.get('mentorOrg'),
-              email: formData.get('mentorEmail'),
-              phoneNumber: formData.get('mentorPhone')
-            },
-            shortDescription: formData.get('mentorOrg'),
-            fullReport: formData.get('fullReport')
-          };
-        }
+    const mentorNames = formData.get('mentor').split(" ")
+    const name = user.name.split(' ');
+    const student = students.find(s => s.firstName == name[0] && s.lastName == name[1] )
+    const projectObject = {
+        studentId: student.studentId,
+        subject: formData.get('subject'),
+        year: new Date().getFullYear(), 
+        tags: formData.getAll('selected'),
+        mentor: {
+          firstName: mentorNames[0],
+          lastName: mentorNames[1],
+          organization: formData.get('mentorOrg'),
+          email: formData.get('mentorEmail'),
+          phoneNumber: formData.get('mentorPhone')
+        },
+        shortDescription: formData.get('mentorOrg'),
+        fullReport: formData.get('fullReport')
+      };
+    }
         
-}
