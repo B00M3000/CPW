82c69e4b0cb7786320dd597340ce29524e9472c0
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-09-06 08:26:09 -0400

    shit happened

--- src/routes/(authenticated)/project/[id]/+page.svelte
+++ src/routes/(authenticated)/project/[id]/+page.svelte
@@ -1,7 +1,8 @@
 <script lang=ts>
     import projects from "@client/data/generated/projects.json";
+    const project = projects.find((id:string) => id == id) 
 
-    const project = projects.find((id:string) => id == id) //some logic for searching for project... how to get id of current project from link?
+
 </script>
 
 
---
+++ src/server/mongo/schemas/project.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+
+
+interface ProjectDocumentData {
+  studentId: string;
+  title: string;
+  year: number;
+  tags: string[];
+
+  mentorId: string;
+  //shortDesc: string;
+  //fullReport: string;
+}
+
+const schema = new mongoose.Schema(
+  {
+    studentId: { type: String, required: true },
+    title: { type: String, required: true },
+    year: { type: Number, required: true },
+    tags: { type: [String], required: true },
+    mentorId: { type: String, required: true },
+    shortDesc: { type: String, required: true },
+    fullReport: { type: String, required: true }
+  },
+  { timestamps: true },
+);
+export type ProjectDocument = mongoose.HydratedDocument<ProjectDocumentData>;
+
+export const ProjectSchema: mongoose.Model<ProjectDocumentData> =
+  mongoose.models['Projects'] || mongoose.model('Projects', schema);
