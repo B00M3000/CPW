8ba2b39e4917db1ea7ae79bfd59a68c35bec3df3
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-09-07 13:50:58 -0400

    remove custom

--- src/routes/(authenticated)/project/create/+page.svelte
+++ src/routes/(authenticated)/project/create/+page.svelte
@@ -18,16 +18,16 @@
 
 <main class="formbar">
 
-  <div class="custom-form-container">
-    <form id="inputForm" class="custom-text-white" method="POST">
+  <div class="form-container">
+    <form id="inputForm" method="POST">
 
-      <div class="custom-form-group">
-        <label for="subject" class="custom-label">Project Title</label>
-        <input type="text" id="subject" name="subject" required class="custom-input" bind:value={subject}>
+      <div class="form-group">
+        <label for="subject" class="label">Project Title</label>
+        <input type="text" id="subject" name="subject" required  bind:value={subject}>
       </div>
 
-      <div class="custom-form-group">
-        <label for="selected" class="custom-label">Select Tags</label>
+      <div class="form-group">
+        <label for="selected" class="label">Select Tags</label>
         {#each Object.entries(tags) as [id, label]}
               <input type="checkbox" value = {id} id="selected" name="selected" />
               {label}
@@ -35,42 +35,42 @@
           {/each}
       </div>
       
-      <div class="custom-form-group">
-        <label for="mentorFirst" class="custom-label">Mentor First Name</label>
-        <input type="text" id="mentorFirst" name="mentorFirst" required class="custom-input" bind:value={mentor}>
+      <div class="form-group">
+        <label for="mentorFirst" class="label">Mentor First Name</label>
+        <input type="text" id="mentorFirst" name="mentorFirst" required bind:value={mentor}>
       </div>
 
-      <div class="custom-form-group">
-        <label for="mentorLast" class="custom-label">Mentor Last Name</label>
-        <input type="text" id="mentorLast" name="mentorLast" required class="custom-input" bind:value={mentor}>
+      <div class="form-group">
+        <label for="mentorLast" class="label">Mentor Last Name</label>
+        <input type="text" id="mentorLast" name="mentorLast" required bind:value={mentor}>
       </div>
 
-      <div class="custom-form-group">
-        <label for="mentorOrg" class="custom-label">Mentor Organization</label>
-        <input type="email" id="mentorOrg" name="mentorOrg" required class="custom-input" bind:value={mentorOrg}>
+      <div class="form-group">
+        <label for="mentorOrg" class="label">Mentor Organization</label>
+        <input type="email" id="mentorOrg" name="mentorOrg" required bind:value={mentorOrg}>
       </div>
 
-      <div class="custom-form-group">
-        <label for="mentorEmail" class="custom-label">Mentor Email</label>
-        <input type="email" id="mentorEmail" name="mentorEmail" required class="custom-input" bind:value={mentorEmail}>
+      <div class="form-group">
+        <label for="mentorEmail" class="label">Mentor Email</label>
+        <input type="email" id="mentorEmail" name="mentorEmail" required bind:value={mentorEmail}>
       </div>
 
-      <div class="custom-form-group">
-        <label for="mentorPhone" class="custom-label">Mentor Phone</label>
-        <input type="text" id="mentorPhone" name="mentorPhone" required class="custom-input" bind:value={mentorPhone}>
+      <div class="form-group">
+        <label for="mentorPhone" class="label">Mentor Phone</label>
+        <input type="text" id="mentorPhone" name="mentorPhone" required bind:value={mentorPhone}>
       </div>
 
-      <div class="custom-form-group">
-        <label for="shortDesc" class="custom-label">Write A Short Description</label>
-        <textarea class="custom-textarea" id="shortDesc" name="shortDesc" rows="3" cols="60" maxlength="200" required bind:value={shortDesc}></textarea>
+      <div class="form-group">
+        <label for="shortDesc" class="label">Write A Short Description</label>
+        <textarea id="shortDesc" name="shortDesc" rows="3" cols="60" maxlength="200" required bind:value={shortDesc}></textarea>
       </div>
 
 
       
     </form>
 
-    <div class="custom-form-group custom-button-group">
-      <button type="submit" class="custom-submit-button"> Submit Form</button>
+    <div class="form-group button-group">
+      <button type="submit" class="submit-button"> Submit Form</button>
     </div>
     
   </div>
@@ -81,17 +81,16 @@
   background-color: #4a5568;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
   width: auto; 
-  width: calc(100vw - (100vw - 100%));
   display: flex;
   justify-content: center;
 }
 
-.custom-form-group {
+.form-group {
   padding-top: 2rem;
 }
 
 
-.custom-form-container {
+.form-container {
   
   padding-top: 1.5rem;
 }
@@ -100,15 +99,15 @@
   color: #fff;
 }
 
-.custom-label {
+.label {
   display: block;
   font-size: 0.875rem;
   font-weight: 700;
   margin-bottom: 0.2rem;
 }
 
-.custom-input,
-.custom-textarea {
+input,
+textarea {
   padding: 0.3rem 0.7rem;
   border: 2px solid #000;
   border-radius: 0.375rem;
@@ -118,14 +117,14 @@
   background-color: #525252;
 }
 
-.custom-input:focus,
-.custom-textarea:focus {
+input:focus,
+textarea:focus {
   box-shadow: 0 0 0 3px rgba(59, 130, 246, 0.5);
   border-color: #4299e1;
 }
 
 
-.custom-submit-button {
+.submit-button {
   padding: 0.5rem 1rem;
   background-color: #2563eb;
   color: #fff;
@@ -134,11 +133,11 @@
   transition: background-color 0.2s;
 }
 
-.custom-submit-button:hover {
+.submit-button:hover {
   background-color: #1e40af;
 }
 
-.custom-submit-button:focus {
+.submit-button:focus {
   outline: none;
 }
 
