a9c00a30dc56f33c39d320986b90c358ca85d826
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-09-08 20:19:46 -0400

    t

--- src/routes/(authenticated)/manage-projects/create/+server.ts
+++ src/routes/(authenticated)/manage-projects/create/+server.ts
@@ -1,25 +1,32 @@
-import students from '@/client/data/generated/students.json';
-import { user } from "@/client/stores/user";
+// import students from '@/client/data/generated/students.json';
+// import { user } from "@/client/stores/user";
 
-export async function POST({ request }) {
-  const formData = await request.formData();
-  const mentorNames = formData.get('mentor').split(" ")
-  const name = user.name.split(' ');
-  const student = students.find(s => s.firstName == name[0] && s.lastName == name[1])
-  const projectObject = {
-    studentId: student.studentId,
-    subject: formData.get('subject'),
-    year: new Date().getFullYear(),
-    tags: formData.getAll('selected'),
-    mentor: {
-      firstName: mentorNames[0],
-      lastName: mentorNames[1],
-      organization: formData.get('mentorOrg'),
-      email: formData.get('mentorEmail'),
-      phoneNumber: formData.get('mentorPhone')
-    },
-    shortDescription: formData.get('mentorOrg'),
-    fullReport: formData.get('fullReport')
-  };
+// export async function POST({ request }) {
+//   const formData = await request.formData();
+//   const mentorNames = formData.get('mentor').split(" ")
+//   const name = user.name.split(' ');
+//   const student = students.find(s => s.firstName == name[0] && s.lastName == name[1])
+//   const projectObject = {
+//     studentId: student.studentId,
+//     subject: formData.get('subject'),
+//     year: new Date().getFullYear(),
+//     tags: formData.getAll('selected'),
+//     mentor: {
+//       firstName: mentorNames[0],
+//       lastName: mentorNames[1],
+//       organization: formData.get('mentorOrg'),
+//       email: formData.get('mentorEmail'),
+//       phoneNumber: formData.get('mentorPhone')
+//     },
+//     shortDescription: formData.get('mentorOrg'),
+//     fullReport: formData.get('fullReport')
+//   };
+// }
+
+import { stringifyObjectId } from "@/lib/utils";
+import { ProjectSchema } from "@/server/mongo/schemas/project";
+
+export async function load() {
+    const ghosts = (await ProjectSchema.find().lean())?.map(stringifyObjectId);
+    return { ghosts };
 }
-
