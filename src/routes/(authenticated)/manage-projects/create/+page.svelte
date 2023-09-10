e1c09fc86ff60435ca0378ec80f650b1a00e9749
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-09-10 13:08:19 -0400

    new validations

--- src/routes/(authenticated)/manage-projects/create/+page.svelte
+++ src/routes/(authenticated)/manage-projects/create/+page.svelte
@@ -58,61 +58,61 @@
     const nextStep = () => { if(step < maxStep) step += 1 }
     const backStep = () => { if(step > minStep) step -= 1 }
 
+    const phoneNumberRegex = /^\+((?:9[679]|8[035789]|6[789]|5[90]|42|3[578]|2[1-689])|9[0-58]|8[1246]|6[0-6]|5[1-8]|4[013-9]|3[0-469]|2[70]|7|1)(?:\W*\d){0,13}\d$/;
+
+    type validation = () => true | string[];
+
+    const stepValidations: validation[] = [
+      () => {
+        if(/.{24,100}/.test(project.title)) return true;
+        else return ["Please enter a project name between 24 and 200 characters!"];
+      },
+      () => {
+        const numberOfTags= project.tags.length
+        if(numberOfTags >= 1 && numberOfTags <= 5) return true;
+        else return ["Please select between 1 and 5 tags."];
+      },
+      () => {
+        if(action.mentorId) return true;
+       ( nonEmptyRegex.test(mentor.firstName) && nonEmptyRegex.test(mentor.lastName) && )
+      }
+    ]
+
     function mentorSelected(e: CustomEvent) {
       const { mentorId } = e.detail;
       action.mentorId = mentorId;
     }
 </script>
 
-<main class="formbar">
-  <form class="form-container">
+<main>
+  <form>
     {#if step === 1}
-    <div class="form-group">
-      <label for="subject" class="label">Project Title</label>
-      <input type="text" id="title" maxlength="120" minlength="5"  required bind:value={project.title}>
-    </div>
+    <label for="subject" class="label">Project Title</label>
+    <input type="text" id="title" required bind:value={project.title}>
     {:else if step === 2}
-    <div class="form-group">
-      <label for="selected" class="label">Select Tags</label>
-      {#each Object.entries(tags) as [id, label]}
-      <input type="checkbox" value={id} id="selected" bind:group={project.tags}/>
-      {label}
-      <br />
-      {/each}
-    </div>
+    <label for="selected" class="label">Select Tags</label>
+    {#each Object.entries(tags) as [id, label]}
+    <input type="checkbox" value={id} id="selected" bind:group={project.tags}/>
+    {label}
+    <br />
+    {/each}
     {:else if step === 3}
     <MentorSearcher on:select={mentorSelected}/>
     {#if !action.mentorId}
-    <div class="form-group">
-      <label for="mentorFirst" class="label">Mentor First Name</label>
-      <input type="text" id="mentorFirstName" required bind:value={mentor.firstName}>
-    </div>
-  
-    <div class="form-group">
-      <label for="mentorLast" class="label">Mentor Last Name</label>
-      <input type="text" id="mentorLastName" required bind:value={mentor.lastName}>
-    </div>
-
-    <div class="form-group">
-      <label for="mentorOrg" class="label">Mentor Organization</label>
-      <input type="email" id="mentorOrganization" required bind:value={mentor.organization}>
-    </div>
-
-    <div class="form-group">
-      <label for="mentorEmail" class="label">Mentor Email</label>
-      <input type="email" id="mentorEmail" required bind:value={mentor.email}>
-    </div>
-
-    <div class="form-group">
-      <label for="mentorPhone" class="label">Mentor Phone</label>
-      <input type="text" id="mentorPhone" required bind:value={mentor.phoneNumber}>
-    </div>
+    <label for="mentorFirst" class="label">Mentor First Name</label>
+    <input type="text" id="mentorFirstName" required bind:value={mentor.firstName}>
+    <label for="mentorLast" class="label">Mentor Last Name</label>
+    <input type="text" id="mentorLastName" required bind:value={mentor.lastName}>
+    <label for="mentorOrg" class="label">Mentor Organization</label>
+    <input type="email" id="mentorOrganization" required bind:value={mentor.organization}>
+    <label for="mentorEmail" class="label">Mentor Email</label>
+    <input type="email" id="mentorEmail" required bind:value={mentor.email}>
+    <label for="mentorPhone" class="label">Mentor Phone</label>
+    <input type="text" id="mentorPhone" required bind:value={mentor.phoneNumber}>
     {/if}
     {:else if step === 4}
-    <div class="form-group">
-      <label for="shortDesc" class="label">Write A Short Description</label>
-      <textarea id="shortDesc" rows=3 cols=60 maxlength=200 required bind:value={project.shortDesc}></textarea>
-    </div>
+    <label for="shortDesc" class="label">Write A Short Description</label>
+    <textarea id="shortDesc" rows=3 cols=60 maxlength=200 required bind:value={project.shortDesc}></textarea>
     {/if}
 
     {#if step > minStep}
@@ -124,78 +124,13 @@
     {/if}
 
     {#if step === maxStep}
-    <div class="form-group button-group">
-      <button type="submit" class="submit-button" on:click={upload}> Submit Form</button>
-    </div>
+    <button type="submit" class="submit-button" on:click={upload}> Submit Form</button>
     {/if}
   </form>
 </main>
 
-<style>
-  .formbar {
-    background-color: #4a5568;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
-    
-    height: calc(100vh - var(--nav-bar-height)); 
-    display: flex;
-    justify-content: center;
-  }
-
-  .form-group {
-    padding-top: 2rem;
-  }
-
-
-  .form-container {
-    
-    padding-top: 1.5rem;
-  }
-
-    #inputForm {
-      color: #fff;
-    }
-
-  .label {
-    display: block;
-    font-size: 0.875rem;
-    font-weight: 700;
-    margin-bottom: 0.2rem;
-  }
-
-  input,
-  textarea {
-    padding: 0.3rem 0.7rem;
-    border: 2px solid #000;
-    border-radius: 0.375rem;
-    outline: none;
-    transition: box-shadow 0.2s, border-color 0.2s;
-    color: white;
-    background-color: #525252;
-  }
-
-  input:focus,
-  textarea:focus {
-    box-shadow: 0 0 0 3px rgba(59, 130, 246, 0.5);
-    border-color: #4299e1;
-  }
-
-
-  .submit-button {
-    padding: 0.5rem 1rem;
-    background-color: #2563eb;
-    color: #fff;
-    border-radius: 0.375rem;
-    cursor: pointer;
-    transition: background-color 0.2s;
-  }
-
-  .submit-button:hover {
-    background-color: #1e40af;
-  }
-
-  .submit-button:focus {
-    outline: none;
-  }
+<style lang="scss">
+  
 </style>
 
 
