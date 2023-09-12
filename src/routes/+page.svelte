7907bd5525c356f3c1746788e8617d5b0d9f2209
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-09-11 21:39:59 -0400

    fiokdsjfsd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,76 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
+  import ProjectCard from "@/client/components/ProjectCard.svelte";
   import { user } from "@/client/stores/user";
+
+  export let data;
+
+  $: ({ projects } = data);
 </script>
 
 <main>
-
-    <h1 class="title"><strong>Commonwealth School Project Week Database</strong></h1>
-    <p class="subtitle"> Welcome to the Commonwealth School Project Week collection.
-    </p>
-    {#if !$user}
-    <a class="login-button" href="/login">Log In</a>
-    {/if}
+    <div id="primary">
+        <h1 class="title"><strong>Commonwealth School Project Week Database</strong></h1>
+        <p class="subtitle"> Welcome to the Commonwealth School Project Week collection.
+        </p>
+        <a href="/projects">View Completed Projects</a>
+        {#if !$user}
+        <a class="login-button" href="/login">Log In</a>
+        {/if}
         
+    </div>
 
+    <div class="scroll-down">
+        <span>Scroll down to view more...</span>
+        <img src="/assets/scroll-down.png">
+    </div>
+
+    <h1>5 random projects completed by students at Commonwealth.</h1>
+
+    <div id="projects">
+        {#each projects as project}
+            <ProjectCard {project} />
+        {:else}
+            <span>No Projects.</span>
+        {/each}
+    </div>
 </main>
 
-<style>
+<style lang="scss">
     main {
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+    }
+
+    #primary {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
         height: calc(100vh - var(--nav-bar-height));
+    } 
+
+    #projects {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 1em;
+    }
+
+    .scroll-down {
+        position: relative;
+        bottom: 7em;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        img {
+            width: 13rem;
+            height: 4rem;
+        }
     }
 
     .login-button {
@@ -33,6 +82,7 @@
         text-decoration: none;
         color: white
     }
+
     .title {
         font-size: 2rem;
         padding-top: 0.5rem;
@@ -43,6 +93,4 @@
         padding-top: 0.7rem;
         padding-bottom: 0.7rem;
     }
-
-
 </style>
