dea6bf6b4cac1bda36ae191326f7608bb5633018
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-09-11 22:17:29 -0400

    remoivng useless console.logs

--- src/routes/(authenticated)/admin/+page.server.ts
+++ src/routes/(authenticated)/admin/+page.server.ts
@@ -5,6 +5,5 @@ export async function load(){
 
     const projectCountPublished = await ProjectSchema.count({underReview: false});
     const projectCountNotPublished = await ProjectSchema.count({underReview: true})
-    console.log(projectCountPublished, projectCountNotPublished)
     return { projectCountPublished, projectCountNotPublished };
 }
