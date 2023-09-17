21355fbd1a8350e1a0a4ecd68a16e1de905e5a59
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-09-17 16:44:43 -0400

    d

--- src/server/mongo/schemas/asset.ts
+++ src/server/mongo/schemas/asset.ts
@@ -3,12 +3,14 @@ import mongoose from 'mongoose';
 interface AssetDocumentData {
     contentType: string;
     data: Buffer;
+    ownerId: string;
 }
 
 const schema = new mongoose.Schema(
   {
     contentType: { type: String, required: true },
-    data: { type: Buffer, required: true }
+    data: { type: Buffer, required: true },
+    ownerId: { type: String, required: true }
   },
   { timestamps: true },
 );
