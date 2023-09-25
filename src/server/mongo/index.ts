ab21304d7aad7bcb66470655fef87a0aa62245a7
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-09-24 21:30:03 -0400

    d

--- src/server/mongo/index.ts
+++ src/server/mongo/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-import { MONGO_URI } from '$env/static/private'
+import { MONGO_URI } from '$env/static/private';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async () => {
