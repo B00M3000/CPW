10060e80f40384b20ea11e269d0bc20352c0e811
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-10-02 13:09:05 -0400

    d

--- src/routes/(authenticated)/+layout.server.ts
+++ src/routes/(authenticated)/+layout.server.ts
@@ -1,7 +1,9 @@
+import { AccountType } from '@/lib/enums.js';
 import { error, redirect } from '@sveltejs/kit';
 
 export async function load({ locals, depends, url }) {
     depends('app:logout');
 
     if(!locals.user) throw redirect(307, `/login?redirect=${encodeURIComponent(url.pathname + url.search)}`);
+    if(locals.user.accountType == AccountType.Unknown) throw error(403, { message: "Account is not registered as a Student or Advisor." })
 };
