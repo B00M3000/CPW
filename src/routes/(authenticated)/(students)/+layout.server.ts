eb69a5a1f17916cc59c180854fbdcfc198360358
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-10-02 13:05:39 -0400

    reorganized routes

--- src/routes/(authenticated)/(students)/+layout.server.ts
+++ src/routes/(authenticated)/(students)/+layout.server.ts
@@ -1,5 +1,6 @@
+import { AccountType } from '@/lib/enums.js';
 import { error } from '@sveltejs/kit';
 
 export async function load({ locals }) {
-    if(!locals.user.accountType) throw error(403, { message: "Only students can access these pages." })
+    if(!locals.user.accountType == AccountType.Student) throw error(403, { message: "Only students can access these pages." })
 };
