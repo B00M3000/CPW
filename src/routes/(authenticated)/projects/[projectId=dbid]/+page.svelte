6c8f23d5af924ab561ba340fbf5e1e9858e1e28c
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-10-03 10:10:09 -0400

    f

--- src/routes/(authenticated)/projects/[projectId=dbid]/+page.svelte
+++ src/routes/(authenticated)/projects/[projectId=dbid]/+page.svelte
@@ -9,6 +9,7 @@
 
     $: ({ project, mentor, student, images } = data);
 
+    let view = 0;
 </script>
 
 
@@ -25,21 +26,30 @@
           <Tag text={getTagString(tag)} />
       {/each}
   </div>
+  <div class="report-image-container">
+    <div class="view-nav">
+        <button class={view==0 ? "active" : ""} on:click = {() => view = 0}> View Full Report</button>
+        <button class={view==1 ? "active" : ""} on:click = {() => view = 1}> View Images</button>
 
-  <div class="report">
-      {project?.fullReport || "Sorry, no report has been published for this project."}
-  </div>
+    </div> 
+    {#if view == 0}
+      <div class="report">
+          {project?.fullReport || "Sorry, no report has been published for this project."}
+      </div>
 
-  <div class="sidebar right">
-      <Gallery {images} projectPage={true} autoHeightAndWidth={true}/> 
-  </div>
+    {:else if view == 1}
+      <div class="gallery-container">
+        <Gallery {images} />
+      </div>
+    {/if}
+  </div>  
 </main>
 
 
 <style lang="scss">
   main {
     display: grid;
-    grid-template-columns: 1fr 2fr 1fr;
+    grid-template-columns: 1fr 3fr;
 
     background-color: #eeeeee;
 
@@ -49,14 +59,18 @@
       display: flex;
       flex-direction: column;  
       padding: 25px;
+      height: calc(100vh - var(--nav-bar-height) - 50px);
 
       &.left {
         word-wrap: break-word;
         overflow-y: auto;
+        
       }
-      &.right {
-        overflow-y: auto;
-      }
+    }
+
+    .view-nav {
+      padding: 5px;
+      background-color: var(--color-blue-grey-200);
     }
 
     .report {
@@ -69,6 +83,43 @@
       padding: 20px; 
       overflow-y: auto;
     }
+
+
+    .report-image-container{
+      max-height: calc(100vh - var(--nav-bar-height));
+      overflow-y: scroll;
+    }
+
+    .gallery-container{
+      background-color: white;
+    }
+
+    button {
+      background-color: white;
+      padding: 3px;
+      border: 1px black solid;
+      width: 10rem;
+      font-weight: 800;
+    }
+    
+    .active{
+      border-bottom: blue 5px solid;
+    }
+
+    .active::after {
+      content: "";
+      display: block;
+      position: absolute;
+      margin-left: 4rem;
+      margin-top: 5px;
+      width: 0;
+      height: 0;
+      border-left: 8px solid transparent;
+      border-right: 8px solid transparent;
+      border-top: 8px solid blue;
+    
+    }
+
   }
 
   @media(max-width: 1200px) {
