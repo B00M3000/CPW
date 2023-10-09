a585a4e14ddff5efe77d7ad102a0083f2eb44a4a
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-10-09 14:03:19 -0400

    Only attempting to authenticate users on pages that require authentication

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,5 @@
 import type { Handle } from '@sveltejs/kit';
 import mongo from '@/server/mongo';
-import cookie from 'cookie';
-import { UserSchema } from '@/server/mongo/schemas/user';
 
 const dbPromise = mongo();
 
@@ -9,33 +7,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 	try {
 		await dbPromise;
 
-		const cookies = cookie.parse(event.request.headers.get('cookie') || '');
-		const session_id = cookies.session_id;
-
-		if (session_id) {
-			const user = await UserSchema.findOne({ sessionId: session_id });
-
-			if (user) {
-				event.locals.user = {
-					id: user?._id.toString(),
-					name: user?.name,
-					email: user?.email,
-					picture: user?.picture,
-					sessionId: user?.sessionId,
-					accountType: user?.accountType,
-					accessLevel: user?.accessLevel,
-					adviseeIds: user?.adviseeIds
-				};
-			} else {
-				//TBA: clear session cookie
-			}
-		} else {
-			//TBA: clear session cookie
-		}	
-
-		const response = await resolve(event);
-
-		return response;
+		return await resolve(event);
 	} catch (error) {
 		console.error(error);
 
