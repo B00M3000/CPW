734d48af2f5cdfa64447431c845bf628feae9ace
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-10-09 14:02:14 -0400

    Just use mongoose default connection function

--- src/server/mongo/index.ts
+++ src/server/mongo/index.ts
@@ -3,9 +3,6 @@ import mongoose from 'mongoose';
 import { MONGO_URI } from '$env/static/private';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default async () => {
-  if (mongoose.connections.length > 0) {
-    await mongoose.disconnect();
-  }
-  await mongoose.connect(MONGO_URI);
-};
+export default async function() {
+  return await mongoose.connect(MONGO_URI)
+}
