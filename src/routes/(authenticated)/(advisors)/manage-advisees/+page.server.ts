40b4280025ae445210499fd72a96d6d076a0de7d
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-11-14 13:09:14 -0500

    Update +page.server.ts

--- src/routes/(authenticated)/(advisors)/manage-advisees/+page.server.ts
+++ src/routes/(authenticated)/(advisors)/manage-advisees/+page.server.ts
@@ -4,18 +4,15 @@
  * Copyright (c) 2023 Thomas Zhou
  */
 
-import { AccessLevel } from '@/lib/enums';
 import { UserSchema } from '@/server/mongo/schemas/user.js';
 import { stringifyObjectId } from '@/lib/utils';
 import { ProjectSchema } from '@/server/mongo/schemas/project.js';
 
 export async function load({ locals }) {
-    const students = [];
-    
-    locals.user?.adviseeIds.forEach(adviseeId => {
+    const students = await Promise.all(locals.user.adviseeIds.map(async adviseeId => {
         const counter = await ProjectSchema.count({ studentId: adviseeId, underReview: true })
-        students.push({ student: stringifyObjectId(await UserSchema.findById(adviseeId).lean()), counter });
-    })
+        return { student: stringifyObjectId(await UserSchema.findOne({ schooolId: adviseeId }).lean()), counter };
+    }))
 
-    return { students };
+    return { students }; 
 };
