006cf35aaf09b016cc9013f507e91d61aa1e7d86
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2024-01-28 12:03:39 -0500

    fixzed image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
     <meta property="og:title" content="Project Week Database" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://cpw.tzhou.xyz" />
-    <meta property="og:image" content="https://cpw.tzhou.xyz/CommonwealthSchool.jpg" />
+    <meta property="og:image" content="https://cpw.tzhou.xyz/assets/CommonwealthSchool.jpg" />
     <meta property="og:description" content="This is the database housing records of Project Week projects. Find inspiration from and look at projects completed in previous years." />
     <meta property="og:determiner" content="the" />
     <meta property="og:locale" content="en_GB" />
@@ -23,7 +23,7 @@
     <meta name="twitter:site" content="@commschool">
     <meta name="twitter:title" content="Project Week Database">
     <meta name="twitter:description" content="This is the database housing records of Project Week projects. Find inspiration from and look at projects completed in previous years.">
-    <meta name="twitter:image" content="https://cpw.tzhou.xyz/CommonwealthSchool.jpg">
+    <meta name="twitter:image" content="https://cpw.tzhou.xyz/assets/CommonwealthSchool.jpg">
 </svelte:head>
 
 <Nav />
