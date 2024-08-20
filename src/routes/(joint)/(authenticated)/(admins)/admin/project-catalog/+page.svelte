42fa5df56b679d6390ced9fd9e08e1eea470844c
Thomas Zhou <40374754+B00M3000@users.noreply.github.com>
2024-08-19 23:29:26 -0400

    Update +page.svelte
    Fixed table needing tbody in svelte5 issue
    
    Signed-off-by: Thomas Zhou <40374754+B00M3000@users.noreply.github.com>

--- src/routes/(joint)/(authenticated)/(admins)/admin/project-catalog/+page.svelte
+++ src/routes/(joint)/(authenticated)/(admins)/admin/project-catalog/+page.svelte
@@ -6,9 +6,9 @@
 
 <script lang=ts>
     import { goto } from "$app/navigation";
-  import Icon from "@/client/components/Icon.svelte";
+    import Icon from "@/client/components/Icon.svelte";
     import StudentCard from "@/client/components/StudentCard.svelte";
-  import Open from "@/client/icons/Open";
+    import Open from "@/client/icons/Open";
 
     export let data;
 
@@ -16,7 +16,6 @@
 
     $: completedProjects = inflatedProjects.filter((p) => p.underReview == false);
     $: uncompletedProjects = inflatedProjects.filter((p) => p.underReview == true);
-
 </script>
 
 <main class="flex flex-row justify-around mt-10">
@@ -24,32 +23,35 @@
         <h1 class="text-center text-2xl m-4">Unapproved Projects</h1>
         <div class="max-h-[75vh] overflow-y-auto">
             <table class="inline-table">
-                <tr>
-                    <th></th>
-                    <th>
-                        <span>Title</span>   
-                    </th>
-                    <th>
-                        <span>Student </span>
-                    </th>
-                </tr>
-            
-                {#each uncompletedProjects as project}
-                <tr on:click ={()  => goto(`project-catalog/${project._id}`)}>
-                    <td>
-                        <button class="rounded-md bg-slate-300 flex gap-2 items-center justify-center p-1 hover:bg-slate-400" on:click={() => window.open(`/admin/project-catalog/${project._id}`, "_blank")}>
-                            <Icon src={Open} size="1.5rem"/>
-                            <span>Open</span>
-                        </button>
-                    </td>
-                    <td>
-                        <p class="max-w-[24rem]">{project.title}</p>
-                    </td>
-                    <td>
-                        <span> <StudentCard student={project.student} /> </span>
-                    </td>
-                </tr>
-                {/each}
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>
+                            <span>Title</span>   
+                        </th>
+                        <th>
+                            <span>Student </span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {#each uncompletedProjects as project}
+                    <tr on:click ={()  => goto(`project-catalog/${project._id}`)}>
+                        <td>
+                            <button class="rounded-md bg-slate-300 flex gap-2 items-center justify-center p-1 hover:bg-slate-400" on:click={() => window.open(`/admin/project-catalog/${project._id}`, "_blank")}>
+                                <Icon src={Open} size="1.5rem"/>
+                                <span>Open</span>
+                            </button>
+                        </td>
+                        <td>
+                            <p class="max-w-[24rem]">{project.title}</p>
+                        </td>
+                        <td>
+                            <span> <StudentCard student={project.student} /> </span>
+                        </td>
+                    </tr>
+                    {/each}
+                </tbody>
             </table>
         </div>
     </div>
