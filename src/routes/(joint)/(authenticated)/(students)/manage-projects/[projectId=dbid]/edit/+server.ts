7813b14eff35c7144f2165f501473bb697d40e23
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2024-08-27 21:58:04 +0000

    updated project edit data validation

--- src/routes/(joint)/(authenticated)/(students)/manage-projects/[projectId=dbid]/edit/+server.ts
+++ src/routes/(joint)/(authenticated)/(students)/manage-projects/[projectId=dbid]/edit/+server.ts
@@ -7,24 +7,28 @@
 import { ProjectSchema } from "@/server/mongo/schemas/project";
 import { error, json } from "@sveltejs/kit";
 import { sanitizeFilter } from "mongoose";
+import { validateProjectShortDescription, validateProjectTags, validateProjectTitle } from "../../create/server-validation";
 
 export async function POST({ request, params: { projectId } }) {
     const {
         project: { shortDesc, tags, title },
     } = await request.json();
 
-    if (!(title.length < 100 && title.length > 12))
-        return error(
-            400,
-            "Please enter a project name between 12 and 200 characters.",
-        );
-    if (!(tags.length >= 1 && tags.length <= 7))
-        return error(400, "Please select between 1 and 7 tags.");
-    if (!(shortDesc.length > 100))
-        return error(
-            400,
-            "Please enter a short description with at least 100 characters to start. You can always edit it later.",
-        );
+    // Validate project details
+    let validationResult = validateProjectTitle(title);
+    if (validationResult !== true) {
+        return error(400, validationResult.join(" "));
+    }
+
+    validationResult = validateProjectTags(tags);
+    if (validationResult !== true) {
+        return error(400, validationResult.join(" "));
+    }
+
+    validationResult = validateProjectShortDescription(shortDesc);
+    if (validationResult !== true) {
+        return error(400, validationResult.join(" "));
+    }
 
     await ProjectSchema.findOneAndUpdate(sanitizeFilter({ _id: projectId }), {
         title,
