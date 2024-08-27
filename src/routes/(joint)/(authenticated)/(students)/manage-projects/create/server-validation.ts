3c7b4cc31f7b8717663091a0e80ce0571d54d521
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2024-08-27 21:52:50 +0000

    fix regex DOS issue

--- src/routes/(joint)/(authenticated)/(students)/manage-projects/create/server-validation.ts
+++ src/routes/(joint)/(authenticated)/(students)/manage-projects/create/server-validation.ts
@@ -30,10 +30,8 @@ export function validateProjectShortDescription(
 export function validateMentorInformation(
     mentorInformation: any,
 ): true | string[] {
-
-    const EMAIL_REGEX =
-        // eslint-disable-next-line no-control-regex
-        /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    // eslint-disable-next-line no-control-regex
+    const EMAIL_REGEX = /.+@.+\..+/;
     const PHONE_REGEX =
         /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
 
