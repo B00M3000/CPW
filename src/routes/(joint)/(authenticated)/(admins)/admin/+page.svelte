88b4ef83cbb0267a77f566138bd3087380877796
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2024-09-14 01:45:39 +0000

    Obliterated the old data upload flow

--- src/routes/(joint)/(authenticated)/(admins)/admin/+page.svelte
+++ src/routes/(joint)/(authenticated)/(admins)/admin/+page.svelte
@@ -9,10 +9,6 @@
     import { user } from "@/client/stores/user";
     import { onMount } from "svelte";
 
-    async function gotoDataUpload() {
-        await goto('/admin/data-upload');
-    }
-
     async function gotoMentorSearch() {
       await goto('/admin/mentor-search')
     }
@@ -38,7 +34,7 @@
 
     //     const animatePub = () => {
     //         const value = projectCountPublished;
-            
+
     //         const time = value / speed;
     //         if(countPub < value) {
     //             countPub = Math.ceil(countPub + time);
@@ -47,13 +43,13 @@
     //         }else{
     //             countPub = value;
     //         }
-            
+
     //     }
 
     //     const animateUnPub = () => {
     //         const value = projectCountNotPublished;
     //         const data = countNotPub;
-            
+
     //         const time = value / speed;
     //         if(countNotPub < value) {
     //             countNotPub = Math.ceil(countNotPub + time);
@@ -63,7 +59,7 @@
     //             countNotPub = value;
     //         }
     //     }
-   
+
     //     animatePub();
     //     animateUnPub();
     // });
@@ -76,7 +72,7 @@
         <div class="dashboard-panel-head">
             <h1 class="title"><strong>Administrator Dashboard</strong></h1>
         </div>
-        
+
         <div class="dashboard-panel">
             <div class="count-panel">
                 <h3 class="count-title">Published Projects</h3>
@@ -90,14 +86,14 @@
         </div>
 
         <div class="dashboard-panel-button">
-          <button on:click={gotoDataUpload} class="dashboard-button">Data Upload</button>
+          <!-- <button on:click={gotoDataUpload} class="dashboard-button">Data Upload</button> -->
           <button on:click={gotoMentorSearch} class="dashboard-button">Mentor Searcher</button>
           <button on:click={gotoProjectCatalog} class="dashboard-button">Project Catalog</button>
           <button on:click={gotoYearStatus} class="dashboard-button">Year Status</button>
         </div>
     </div>
 </main>
-  
+
 <style>
  main {
     display: flex;
@@ -109,35 +105,35 @@
   }
 
   .dashboard-panel-head {
-    background-color: #858585; 
-    border: 1px solid #1E1E1E; 
+    background-color: #858585;
+    border: 1px solid #1E1E1E;
     border-radius: 5px;
     padding: 20px;
     margin: 10px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2); 
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
   }
   .dashboard-panel {
-    background-color: #858585; 
+    background-color: #858585;
     display: flex;
     gap: 2rem;
-    border: 1px solid #1E1E1E; 
+    border: 1px solid #1E1E1E;
     border-radius: 5px;
     padding: 20px;
     margin: 10px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2); 
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
   }
 
   .dashboard-panel-button {
-    background-color: #858585; 
+    background-color: #858585;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    border: 1px solid #1E1E1E; 
+    border: 1px solid #1E1E1E;
     border-radius: 5px;
     padding: 20px;
     margin: 10px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2); 
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
   }
 
   .title {
@@ -160,11 +156,11 @@
 
   .count-value {
     font-size: 2.8rem;
-    color: var(--color-red-400); 
+    color: var(--color-red-400);
   }
 
   .dashboard-button {
-    background-color: #0073E6; 
+    background-color: #0073E6;
     color: #fff;
     padding: 10px 20px;
     font-size: 1.2rem;
@@ -177,11 +173,11 @@
   }
 
   .dashboard-button:hover {
-    background-color: #005CBF; 
+    background-color: #005CBF;
   }
   .main-container{
-    background-color: var(--color-blue-grey-100); 
-    border: 1px solid #1E1E1E; 
+    background-color: var(--color-blue-grey-100);
+    border: 1px solid #1E1E1E;
     border-radius: 5px;
     padding: 20px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
@@ -189,4 +185,3 @@
   }
 
 </style>
-
