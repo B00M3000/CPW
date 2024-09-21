ce82b5c3e4764d3be7338ee0ee314f177ead21e6
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2024-09-21 19:16:55 +0000

    Added Mobile Filter to all non-public pages

---
+++ src/client/components/MobileDeviceFilter.svelte
@@ -0,0 +1,19 @@
+<script lang=ts>
+    import { browser } from "$app/environment";
+
+    let { children } = $props();
+
+    let windowWidth = $state(99999);
+    if(browser) windowWidth = window.innerWidth;
+</script>
+
+{#if windowWidth > 640}
+{@render children()}
+{:else}
+<div class="w-full h-full flex flex-col items-center justify-center gap-8">
+    <img src=/assets/desktop.png class="w-48 h-48" alt="desktop"/>
+    <span class="text-2xl mx-8 text-center">Please visit this page on a desktop or bigger screen.</span>
+</div>
+{/if}
+
+<svelte:window bind:innerWidth={windowWidth} />
