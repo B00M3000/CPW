944344a343a6c360476699cf1e3ec751ea66bc6a
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2025-02-07 01:08:41 +0000

    updated /projects page

--- src/routes/(joint)/(authenticated)/projects/+page.svelte
+++ src/routes/(joint)/(authenticated)/projects/+page.svelte
@@ -98,7 +98,7 @@
 <Loading2 />
 {/if}
 
-<main class="h-full w-full grid grid-cols-1 grid-rows-[auto_minmax(0,_1fr)_auto] bg-gray-300 overflow-hidden">
+<main class="h-full w-full grid grid-cols-1 gap-4 grid-rows-[auto_minmax(0,_1fr)_auto] bg-gray-100 overflow-hidden">
     {#if refinedSearchMobileOpenF}
     <div class="relative">
         <div class="absolute top-0 w-screen z-10">
@@ -124,8 +124,8 @@
         {/if}
         {#if inflatedProjects.length > 0}
         <div class="sm:rounded-lg h-full w-full bg-gray-400">
-            <div class="h-full w-full overflow-y-scroll p-8" bind:this={scrollElement}>
-                <div class="grid gap-8 grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] grid-rows-auto justify-center items-center h-full w-full shadow-lg">
+            <div class="h-full w-full overflow-y-scroll" bind:this={scrollElement}>
+                <div class="p-8 grid gap-8 grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] grid-rows-auto justify-center items-center w-full shadow-lg">
                     {#each inflatedProjects as project}
                     <div class="flex items-center justify-center w-full h-full">
                         <ProjectCard {project} />
