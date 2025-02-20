2c01b08f48e64e6e4e5feee3eb1600f132788f74
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2025-02-20 02:19:53 +0000

    Fixed spelling of Adivsee and the formatting on the Advisee projects page

--- src/routes/(joint)/(authenticated)/(advisors)/manage-advisees/+page.svelte
+++ src/routes/(joint)/(authenticated)/(advisors)/manage-advisees/+page.svelte
@@ -7,6 +7,7 @@
 <script lang="ts">
     import { goto, invalidate } from "$app/navigation";
     import { ErrorFilled, WarningAltFilled, CheckmarkFilled } from "carbon-icons-svelte";
+    import { Table, TableHead, TableHeadCell, TableBody, TableBodyRow, TableBodyCell } from "flowbite-svelte";
 
     let { data } = $props();
     let { students } = $derived(data);
@@ -21,74 +22,62 @@
     }
 </script>
 
-<main class="w-full h-full overflow-y-auto p-4">
-    <h1 class="text-4xl text-center mb-5 p-10 bg-slate-300 rounded-lg">Your Advisees</h1>
-    <div class="max-md:flex max-md:flex-col max-sm:gap-5 md:grid md:grid-cols-[auto_auto_auto] sm:gap-2 sm:mx-12">
-        {#each students as student}
-        <div class="md:grid max-md:grid-rows-3 md:col-span-full md:grid-cols-subgrid max-md:flex max-md:flex-col gap-5 shadow-md bg-white rounded-lg p-4 sm:p-6 items-center">
-            <div class="flex flex-col items-start">
-                {#if student.name}
-                <span class="text-lg mb-2">{student.name}</span>
-                {:else}
-                <span class="text-lg mb-2">[has not logged in]</span>
-                {/if}
-                <span class="text-base text-gray-600">{student.email}</span>
+<main class="sm:p-8 p-4 flex flex-col bg-white items-center">
+    <div class="max-w-[56rem] w-full">
+        <h1 class="text-4xl text-center mb-5 p-10 bg-gray-300 rounded-lg">Your Advisees</h1>
+        <div class="flex items-center flex-col px-8 lg:px-16 w-full">
+            <div class="shadow-xl w-full">
+                <Table striped={true} items={students}>
+                    <TableHead class="bg-gray-200 border-2">
+                        <TableHeadCell>Student</TableHeadCell>
+                        <TableHeadCell>Status</TableHeadCell>
+                        <TableHeadCell></TableHeadCell>
+                    </TableHead>
+                    <TableBody>
+                        <TableBodyRow slot="row" let:item class="border-2">
+                            {@const student = item}
+                            <TableBodyCell>
+                                <div class="flex flex-col items-start">
+                                    {#if student.name}
+                                    <span class="text-lg mb-2">{student.name}</span>
+                                    {:else}
+                                    <span class="text-lg mb-2">[has not logged in]</span>
+                                    {/if}
+                                    <span class="text-base text-gray-600">{student.email}</span>
+                                </div>
+                            </TableBodyCell>
+                            <TableBodyCell>
+                                <div class="flex items-center gap-2">
+                                    {#if !student.currentPending}
+                                    <ErrorFilled color="red" size={24} />
+                                    {:else if !student.currentApproved}
+                                    <WarningAltFilled color="#C96" size={24} />
+                                    {:else}
+                                    <CheckmarkFilled color="green" size={24} />
+                                    {/if}
+                                    <span class='text-lg'>{!student.currentPending ? "No project created" : !student.currentApproved ? "Awaiting approval" : "Complete"}</span>
+                                </div>
+                            </TableBodyCell>
+                            <TableBodyCell>
+                                <div class="flex items-start">
+                                    <button on:click={() => goto(`manage-advisees/${student._id}`)} class="relative p-3 rounded-lg my-4 text-white bg-blue-500 hover:bg-blue-600" class:bg-slate-400={student.currentApproved}>
+                                        <span>View Advisee</span>
+                                        {#if !student.currentApproved}
+                                            <span class="button-badge">!</span>
+                                        {/if}
+                                    </button>
+                                </div>
+                            </TableBodyCell>
+                        </TableBodyRow>
+                    </TableBody>
+                </Table>
             </div>
-            <div class="flex flex-col items-start">
-                <div class="flex items-center gap-2">
-                    {#if !student.currentPending}
-                    <ErrorFilled color="red" size={24} />
-                    {:else if !student.currentApproved}
-                    <WarningAltFilled color="#C96" size={24} />
-                    {:else}
-                    <CheckmarkFilled color="green" size={24} />
-                    {/if}
-                    <span class='text-lg'>{!student.currentPending ? "No project created" : !student.currentApproved ? "Awaiting approval" : "Complete"}</span>
-                </div>
-            </div>
-            <button on:click={() => goto(`manage-advisees/${student._id}`)} class="advisee-button justify-self-center">
-                <span>View Advisee</span>
-                {#if !student.currentApproved}
-                    <span class="button-badge">!</span>
-                {/if}
-            </button>
         </div>
-        {/each}
-    </div>
-</main>
-
-<main class="sm:p-8 p-4">
-    <h1 class="text-4xl text-center mb-5 p-10 bg-gray-300 rounded-lg">Your Advisees</h1>
-    <div class="">
-        
     </div>
 </main>
 
 
 <style lang="scss">
-    .advisee-button {
-        position: relative;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 150px;
-        height: 50px;
-        color: #333333;
-        background: #a8a8a8;
-        border: none;
-        outline: none;
-        border-radius: 5px;
-        color: white;
-        font-weight: bold;
-        padding: 5px;
-        background-color: rgb(100, 100, 255);
-    }
-
-    .advisee-button:hover {
-        cursor: pointer;
-        background-color: rgb(150, 150, 255);
-    }
-
     .button-badge {
         position: absolute;
         top: -10px;
--- src/routes/(joint)/(authenticated)/(advisors)/manage-advisees/[studentId=dbid]/+page.svelte
+++ src/routes/(joint)/(authenticated)/(advisors)/manage-advisees/[studentId=dbid]/+page.svelte
@@ -32,66 +32,58 @@
 </script>
 
 <main>
-    <h1 class="text-4xl text-center mb-5 p-10 bg-slate-300 rounded-lg">Adivsee Projects</h1>
-    <div class="card-container">
-        {#each projectUnapproved as project}
-            <div class="project-card">
-                <ProjectCard {project} displayLearnMore={false} />
-                <div class="button-container">
-                    <button class="fullreport-button" on:click={() => gotoInspectProject(student._id, project._id)}>
-                        Inspect Project
-                    </button>
+    <h1 class="text-4xl text-center mb-5 p-10 bg-slate-300 rounded-lg">Advisee Projects</h1>
+    <div class="flex flex-col items-center">
+        <div class="gap-4 grid grid-cols-[repeat(auto-fit,_minmax(38rem,_1fr))] auto-rows-fr max-w-[77rem] w-full">
+            {#each projectUnapproved as project}
+                <div class="project-card">
+                    <ProjectCard {project} displayLearnMore={false} />
+                    <div class="button-container">
+                        <button class="fullreport-button" on:click={() => gotoInspectProject(student._id, project._id)}>
+                            Inspect Project
+                        </button>
 
-                    <button class="accept-button" on:click = {() => gotoInspectProject(student._id, project._id)}>
-                        Approve Project
-                    </button>
+                        <button class="accept-button" on:click = {() => gotoInspectProject(student._id, project._id)}>
+                            Approve Project
+                        </button>
+                    </div>
                 </div>
-            </div>
-        {/each}
-        {#each projectsApproved as project}
-        <div class="project-card">
-            <ProjectCard {project} />
-            <div class="button-container">
-                <button class="fullreport-button" on:click={() => gotoInspectProject(student._id, project._id)}>
-                    Inspect Project
-                </button>
+            {/each}
+            {#each projectsApproved as project}
+                <div class="project-card">
+                    <ProjectCard {project} />
+                    <div class="button-container">
+                        <button class="fullreport-button" on:click={() => gotoInspectProject(student._id, project._id)}>
+                            Inspect Project
+                        </button>
 
-                <button class="deny-button" on:click = {() => changeApproval(project, false)}>
-                    Unapprove Project
-                </button>
-            </div>
+                        <button class="deny-button" on:click = {() => changeApproval(project, false)}>
+                            Unapprove Project
+                        </button>
+                    </div>
+                </div>
+            {/each}
         </div>
-        {/each}
     </div>
 </main>
 
 
 <style lang="scss">
     main {
-        padding:1rem 5rem;
+        padding: 1rem 5rem;
 
-        .card-container{
-            display:flex;
-            justify-content: center;
-            align-items: center;
-            margin-top: 2rem;
-            gap: 2rem;
-            justify-content: center;
-            flex-wrap: wrap;
-        }
-
-        .project-card{
+        .project-card {
+            width: 100%;
+            height: 100%;
             background-color: #fff;
-            border: 1px solid #ccc;
             padding: 20px;
-            margin-bottom: 20px;
             border-radius: 10px;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            display: flex;
             display: flex;
             flex-direction: row;
             justify-content: space-between;
             align-items: center;
+            max-width: 40rem;
         }
 
         .button-container {
